d84f3541a8df37e2e81e1add50002642e93a20a9
Raphael Soares <faelsoaresjf@gmail.com>
2024-07-10 15:14:41 -0300

    first commit

---
+++ src/pages/trip-details/guests.tsx
@@ -0,0 +1,55 @@
+import { CircleCheck, CircleDashed, UserCog } from "lucide-react";
+import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+interface Participant {
+    id: string
+    name: string | null
+    email: string
+    is_confirmed: boolean
+}
+
+export function Guests() {
+
+    const { tripId } = useParams()
+    const [participants, setParticipants] = useState<Participant[]>([])
+
+    useEffect(() => {
+        api.get(`/trips/${tripId}/participants`).then(
+            response => setParticipants(response.data.participants)
+        )
+    }, [tripId])
+    return (
+        <div className="space-y-6">
+            <h2 className="text-xl font-semibold">Convidados</h2>
+            <div className="space-y-5">
+                {participants.map((participant, index) => {
+                    return (
+                        <div key={participant.id} className="flex items-center justify-between gap-4">
+                            <div className="space-y-1.5">
+                                <span className="block font-medium text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
+                                <span className="block text-sm text-zinc-400 truncate">
+                                    {participant.email}
+                                </span>
+                            </div>
+                            {participant.is_confirmed ? (
+                                <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+                            ) : (
+                                <CircleCheck className="size-5 text-lime-300 shrink-0" />
+                            )}
+                            
+                        </div>
+                    )
+                })}
+                
+            </div>
+
+            <Button variant="secondary" size="full">
+                <UserCog className="size-5"/>
+                Gerenciar convidados
+            </Button>
+        </div>
+    )
+}
